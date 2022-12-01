29c4b7ccb9bd4ce6c9b59f44fdd2e6f2c4ea9b0f
MatHack <realmathack@users.noreply.github.com>
2022-12-01 08:16:14 +0100

    Day 1

---
+++ 1/1/Program.cs
@@ -0,0 +1,29 @@
+﻿var input = File.ReadAllLines("input.txt");
+var results = new List<int>();
+var total = 0;
+
+foreach (var line in input)
+{
+	if (string.IsNullOrWhiteSpace(line))
+	{
+		results.Add(total);
+		total = 0;
+		continue;
+	}
+	if (int.TryParse(line, out int current))
+	{
+		total += current;
+	}
+	else
+	{
+		Console.WriteLine($"Not an integer value: {line}");
+	}
+}
+
+results = results.OrderByDescending(x => x).ToList();
+
+var highest = results[0];
+var top3 = results[0] + results[1] + results[2];
+
+Console.WriteLine(highest);
+Console.WriteLine(top3);
