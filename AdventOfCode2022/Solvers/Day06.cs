f7cc2d4a7c7b18954ea82fe5884960de31a4467d
MatHack <realmathack@users.noreply.github.com>
2022-12-06 07:57:31 +0100

    Day 6 refactor after reading the HashSet reference page

--- AdventOfCode2022/Solvers/Day06.cs
+++ AdventOfCode2022/Solvers/Day06.cs
@@ -20,29 +20,15 @@ namespace AdventOfCode2022.Solvers
 
         private static int GetMarkerPosition(string data, int charCount)
         {
-            var result = 0;
-            var checkSet = new HashSet<char>(charCount);
             for (int i = 0; i < data.Length - charCount; i++)
             {
-                for (int j = 0; j < charCount; j++)
-                {
-                    if (checkSet.Contains(data[i+j]))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        checkSet.Add(data[i+j]);
-                    }
-                }
+                var checkSet = new HashSet<char>(data.Substring(i, charCount));
                 if (checkSet.Count == charCount)
                 {
-                    result = i + charCount;
-                    break;
+                    return i + charCount;
                 }
-                checkSet.Clear();
             }
-            return result;
+            return 0;
         }
     }
 }
