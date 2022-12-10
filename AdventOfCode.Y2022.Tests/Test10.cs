d031b1505a2acf951d5b37b5a72de20da888f918
MatHack <realmathack@users.noreply.github.com>
2022-12-10 08:29:16 +0100

    Day 10

---
+++ AdventOfCode.Y2022.Tests/Test10.cs
@@ -0,0 +1,181 @@
+namespace AdventOfCode.Y2022.Tests
+{
+    public class Test10
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day10();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(13140, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day10();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(@"
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....", result);
+        }
+
+        private const string _input = @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+";
+    }
+}
---
+++ AdventOfCode.Y2022/Solvers/Day10.cs
@@ -0,0 +1,67 @@
+using System.Text;
+
+namespace AdventOfCode.Y2022.Solvers
+{
+    public class Day10 : SolverWithLines
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var registerX = 1;
+            var signalStrength = 0;
+            var instructions = ToAddInstructions(input);
+            for (int i = 1; i <= 220; i++)
+            {
+                if (i % 40 == 20)
+                {
+                    signalStrength += registerX * i;
+                }
+                if (instructions.TryGetValue(i - 1, out var addition))
+                {
+                    registerX += addition;
+                }
+            }
+            return signalStrength;
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var registerX = 1;
+            var screen = new StringBuilder();
+            var instructions = ToAddInstructions(input);
+            for (int i = 1; i <= 240; i++)
+            {
+                if (i % 40 == 1)
+                {
+                    screen.AppendLine();
+                }
+                screen.Append(IsLit(registerX, (i - 1) % 40) ? '#' : '.');
+                if (instructions.TryGetValue(i - 1, out var addition))
+                {
+                    registerX += addition;
+                }
+            }
+            return screen.ToString();
+        }
+
+        private static Dictionary<int, int> ToAddInstructions(string[] lines)
+        {
+            var instructions = new Dictionary<int, int>();
+            var cycle = 1;
+            foreach (var line in lines)
+            {
+                var parts = line.Split(' ');
+                if (parts.Length == 2)
+                {
+                    instructions.Add(cycle++, int.Parse(parts[1]));
+                }
+                cycle++;
+            }
+            return instructions;
+        }
+
+        private static bool IsLit(int registerX, int position)
+        {
+            return position >= registerX - 1 && position <= registerX + 1;
+        }
+    }
+}
