e358aec3cb67c7d1d2ef062e1d94eedb502f08af
MatHack <realmathack@users.noreply.github.com>
2022-12-13 11:55:48 +0100

    Program.cs cleanup for each year

--- AdventOfCode.Y2018/Program.cs
+++ AdventOfCode.Y2018/Program.cs
@@ -1,7 +1,6 @@
 ﻿global using AdventOfCode;
 
 var year = "2018";
-//var day = "01";
 var day = MainHelper.GetDayFromInput(year);
 var type = Type.GetType($"AdventOfCode.Y{year}.Solvers.Day{day}");
 MainHelper.CreateSolver(type).Run(day);
