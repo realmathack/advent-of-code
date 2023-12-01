e6194fd722a4ad6791e7bc9680e9c57172a8d360
MatHack <realmathack@users.noreply.github.com>
2023-12-01 07:44:12 +0100

    Day 1 of 2023

--- AdventOfCode.Y2023.Tests/Test01.cs
+++ AdventOfCode.Y2023.Tests/Test01.cs
@@ -6,24 +6,37 @@ namespace AdventOfCode.Y2023.Tests
         public void TestPart1()
         {
             var subject = new Day01();
-            subject.SetInput(_input);
+            subject.SetInput(_input1);
 
             var result = subject.SolvePart1();
 
-            Assert.Equal(null!, result);
+            Assert.Equal(142, result);
         }
 
         [Fact]
         public void TestPart2()
         {
             var subject = new Day01();
-            subject.SetInput(_input);
+            subject.SetInput(_input2);
 
             var result = subject.SolvePart2();
 
-            Assert.Equal(null!, result);
+            Assert.Equal(281, result);
         }
 
-        private const string _input = @"";
+        private const string _input1 = @"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+";
+
+        private const string _input2 = @"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+";
     }
 }
--- AdventOfCode.Y2023/Solvers/Day01.cs
+++ AdventOfCode.Y2023/Solvers/Day01.cs
@@ -4,12 +4,59 @@ namespace AdventOfCode.Y2023.Solvers
     {
         public override object SolvePart1(string[] input)
         {
-            return null!;
+            var total = 0;
+            foreach (var line in input)
+            {
+                var numbers = line.Where(char.IsDigit);
+                total += int.Parse($"{numbers.First()}{numbers.Last()}");
+            }
+            return total;
         }
 
         public override object SolvePart2(string[] input)
         {
-            return null!;
+            var total = 0;
+            foreach (var line in input)
+            {
+                var numbers = GetDigits(line);
+                total += int.Parse($"{numbers.First()}{numbers.Last()}");
+            }
+            return total;
+        }
+
+        private readonly static Dictionary<string, char> _textNumbers = new()
+        {
+            { "one", '1' },
+            { "two", '2' },
+            { "three", '3' },
+            { "four", '4' },
+            { "five", '5' },
+            { "six", '6' },
+            { "seven", '7' },
+            { "eight", '8' },
+            { "nine", '9' }
+        };
+        private static List<char> GetDigits(string input)
+        {
+            var result = new List<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsDigit(input[i]))
+                {
+                    result.Add(input[i]);
+                    continue;
+                }
+                var tmp = input[i..];
+                foreach (var textNumber in _textNumbers)
+                {
+                    if (tmp.StartsWith(textNumber.Key))
+                    {
+                        result.Add(textNumber.Value);
+                        break;
+                    }
+                }
+            }
+            return result;
         }
     }
 }
