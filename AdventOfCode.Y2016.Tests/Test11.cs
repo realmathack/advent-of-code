861a7bdeb7be2f8531eff6c04a085aa67b44ecba
MatHack <realmathack@users.noreply.github.com>
2023-12-19 10:55:48 +0100

    Day 11 of 2016

--- AdventOfCode.Y2016.Tests/Test11.cs
+++ AdventOfCode.Y2016.Tests/Test11.cs
@@ -13,17 +13,6 @@ namespace AdventOfCode.Y2016.Tests
             Assert.Equal(11, result);
         }
 
-        [Fact]
-        public void TestPart2()
-        {
-            var subject = new Day11();
-            subject.SetInput(_input);
-
-            var result = subject.SolvePart2();
-
-            Assert.Equal(null!, result);
-        }
-
         private const string _input = @"The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
 The second floor contains a hydrogen generator.
 The third floor contains a lithium generator.
--- AdventOfCode.Y2016/Solvers/Day11.cs
+++ AdventOfCode.Y2016/Solvers/Day11.cs
@@ -1,17 +1,128 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Y2016.Solvers
 {
     public class Day11 : SolverWithLines
     {
-        public override object SolvePart1(string[] input)
+        public override object SolvePart1(string[] input) => new BFS().Search(ToInitialState(input)).Count;
+        public override object SolvePart2(string[] input) => new BFS().Search(ToInitialState(input, true)).Count;
+
+        private static List<State> GetNextStates(State current)
         {
-            // TODO: Implement
-            return null!;
+            var nextStates = new List<State>();
+            var potentialFloors = new[] { current.CurrentFloor - 1, current.CurrentFloor + 1 }.Where(floor => floor >= 0 && floor < 4).ToArray();
+            foreach (var potentialFloor in potentialFloors)
+            {
+                foreach (var chip1 in current.CurrentChips())
+                {
+                    nextStates.Add(current.MoveItemToFloor(chip1, potentialFloor));
+                    foreach (var chip2 in current.CurrentChips().Where(chip => chip != chip1))
+                    {
+                        nextStates.Add(current.MoveItemToFloor(chip1|chip2, potentialFloor));
+                    }
+                }
+                foreach (var generator1 in current.CurrentGenerators())
+                {
+                    nextStates.Add(current.MoveItemToFloor(generator1, potentialFloor));
+                    foreach (var generator2 in current.CurrentGenerators().Where(generator => generator != generator1))
+                    {
+                        nextStates.Add(current.MoveItemToFloor(generator1|generator2, potentialFloor));
+                    }
+                }
+                foreach (var set in current.CurrentMatchingSets())
+                {
+                    nextStates.Add(current.MoveItemToFloor(set, potentialFloor));
+                }
+            }
+            return nextStates.Where(state => state.IsValid()).ToList();
         }
 
-        public override object SolvePart2(string[] input)
+        private static readonly Regex _regex = new(@"(?<Name>[a-z]+)(\-compatible)? (?<Type>generator|microchip)");
+        private static State ToInitialState(string[] input, bool addExtraItems = false)
         {
-            // TODO: Implement
-            return null!;
+            var floors = 0L;
+            var names = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                foreach (var match in (IEnumerable<Match>)_regex.Matches(input[i]))
+                {
+                    int shift;
+                    if ((shift = names.IndexOf(match.Groups["Name"].Value)) == -1)
+                    {
+                        shift = names.Count;
+                        names.Add(match.Groups["Name"].Value);
+                    }
+                    floors |= ((match.Groups["Type"].Value == "microchip") ? 1L : 2L) << (16 * (3 - i) + shift * 2);
+                }
+            }
+            if (addExtraItems)
+            {
+                floors |= 0b1111L << 16 * 3 + names.Count * 2;
+            }
+            return new(0, floors);
+        }
+
+        // every 16 bits is a floor (first set is floor 1, fourth set is floor 4)
+        // every  2 bits on a floor is a matching generator & microchip set
+        // generator = 10
+        // microchip = 01
+        private const long _setMask = 0b11L;
+        private const long _floorMask = 0b1111_1111_1111_1111L;
+        private readonly record struct State(int CurrentFloor, long Floors)
+        {
+            public State MoveItemToFloor(long itemMask, int newFloor)
+            {
+                var floors = Floors;
+                floors ^= itemMask << (16 * (3 - CurrentFloor));
+                floors |= itemMask << (16 * (3 - newFloor));
+                return new(newFloor, floors);
+            }
+
+            public bool IsValid()
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    var floor = Floors >> (16 * i) & _floorMask;
+                    var chipWithoutGenerator = false;
+                    var anyGenerators = false;
+                    while (floor != 0L)
+                    {
+                        var set = floor & _setMask;
+                        chipWithoutGenerator |= (set == 1);
+                        anyGenerators |= (set > 1);
+                        floor >>= 2;
+                    }
+                    if (chipWithoutGenerator && anyGenerators)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            private IEnumerable<long> InnerGetItem(long mask)
+            {
+                var floor = Floors >> (16 * (3 - CurrentFloor)) & _floorMask;
+                while (mask < _floorMask)
+                {
+                    if ((floor & mask) == mask)
+                    {
+                        yield return mask;
+                    }
+                    mask <<= 2;
+                }
+            }
+
+            public IEnumerable<long> CurrentChips() => InnerGetItem(1L);
+            public IEnumerable<long> CurrentGenerators() => InnerGetItem(2L);
+            public IEnumerable<long> CurrentMatchingSets() => InnerGetItem(3L);
+            public bool IsFinished() => Floors <= _floorMask;
+        }
+
+        private class BFS : Graphs.BFS<State>
+        {
+            protected override List<State> FindNeighbors(State node) => GetNextStates(node);
+            protected override bool IsGoal(State node) => node.IsFinished();
         }
     }
 }
