f6aaa3b3a6f6a805abbafb0a816787717ffd5fb2
MatHack <realmathack@users.noreply.github.com>
2024-12-13 13:44:30 +0100

    Day 12A of 2024

---
+++ AdventOfCode.Y2024.Tests/Test12.cs
@@ -0,0 +1,71 @@
+﻿namespace AdventOfCode.Y2024.Tests
+{
+    public class Test12
+    {
+        [Theory]
+        [InlineData(_input1, 140)]
+        [InlineData(_input2, 772)]
+        [InlineData(_input, 1930)]
+        public void TestPart1(string input, int expected)
+        {
+            var subject = new Day12();
+            subject.SetInput(input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(_input1, 80)]
+        [InlineData(_input2, 436)]
+        [InlineData(_input3, 236)]
+        [InlineData(_input4, 368)]
+        [InlineData(_input, 1206)]
+        public void TestPart2(string input, int expected)
+        {
+            var subject = new Day12();
+            subject.SetInput(input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(expected, result);
+        }
+
+        private const string _input = @"RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+";
+        private const string _input1 = @"AAAA
+BBCD
+BBCC
+EEEC
+";
+        private const string _input2 = @"OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+";
+        private const string _input3 = @"EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE
+";
+        private const string _input4 = @"AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA
+";
+    }
+}
---
+++ AdventOfCode.Y2024/Solvers/Day12.cs
@@ -0,0 +1,74 @@
+﻿using Coords = AdventOfCode.Coords<int>;
+
+namespace AdventOfCode.Y2024.Solvers
+{
+    public class Day12 : SolverWithCharGrid
+    {
+        public override object SolvePart1(char[][] grid) => ToRegions(grid).Sum(region => region.Plots.Count * region.Perimeter);
+        public override object SolvePart2(char[][] grid) => ToRegions(grid, true).Sum(region => region.Plots.Count * region.Sides);
+
+        private static List<Region> ToRegions(char[][] grid, bool calculateSides = false)
+        {
+            var regions = new List<Region>();
+            var plotsInRegions = new HashSet<Coords>();
+            var potentials = new Queue<Coords>();
+            potentials.Enqueue(new(0, 0));
+            while (potentials.TryDequeue(out var current))
+            {
+                if (plotsInRegions.Contains(current))
+                {
+                    continue;
+                }
+                var region = new Region(grid[current.Y][current.X]);
+                regions.Add(region);
+                var regionPlots = new Queue<Coords>();
+                regionPlots.Enqueue(current);
+                while (regionPlots.TryDequeue(out var plot))
+                {
+                    if (!region.Plots.Add(plot))
+                    {
+                        continue;
+                    }
+                    var perimeter = 0;
+                    perimeter += CheckPlot(grid, potentials, regionPlots, region, plot.Left);
+                    perimeter += CheckPlot(grid, potentials, regionPlots, region, plot.Up);
+                    perimeter += CheckPlot(grid, potentials, regionPlots, region, plot.Right);
+                    perimeter += CheckPlot(grid, potentials, regionPlots, region, plot.Down);
+                    region.Perimeter += perimeter;
+                }
+                if (calculateSides)
+                {
+                    region.Sides = 4; // TODO: Calculate sides
+                }
+                plotsInRegions.UnionWith(region.Plots);
+            }
+            return regions;
+        }
+
+        private static int CheckPlot(char[][] grid, Queue<Coords> potentials, Queue<Coords> regionPlots, Region region, Coords potential)
+        {
+            if (grid.IsOutOfBounds(potential))
+            {
+                return 1;
+            }
+            if (grid[potential.Y][potential.X] == region.PlantType)
+            {
+                if (!region.Plots.Contains(potential))
+                {
+                    regionPlots.Enqueue(potential);
+                }
+                return 0;
+            }
+            potentials.Enqueue(potential);
+            return 1;
+        }
+
+        private class Region(char plantType)
+        {
+            public char PlantType { get; } = plantType;
+            public HashSet<Coords> Plots { get; set; } = [];
+            public int Perimeter { get; set; }
+            public int Sides { get; set; }
+        }
+    }
+}
