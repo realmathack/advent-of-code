9114dd51f177b4cc07263eeeaad1dad805876ddf
MatHack <realmathack@users.noreply.github.com>
2024-12-14 18:00:03 +0100

    Day 14 of 2024

---
+++ AdventOfCode.Y2024.Tests/Test14.cs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode.Y2024.Tests
+{
+    public class Test14
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day14(11, 7);
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(12, result);
+        }
+
+        private const string _input = @"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+";
+    }
+}
---
+++ AdventOfCode.Y2024/Solvers/Day14.cs
@@ -0,0 +1,100 @@
+﻿using Coords = AdventOfCode.Coords<int>;
+
+namespace AdventOfCode.Y2024.Solvers
+{
+    public class Day14(int _width, int _height) : SolverWithLines
+    {
+        public Day14() : this(101, 103) { }
+
+        public override object SolvePart1(string[] input)
+        {
+            var robots = ToRobots(input);
+            for (int i = 0; i < 100; i++)
+            {
+                MoveRobots(robots);
+            }
+            return CalculateSafetyFactor(robots);
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var robots = ToRobots(input);
+            for (int i = 0; i < _width * _height; i++)
+            {
+                if (RobotsClustered(robots))
+                {
+                    var image = GenerateImage(robots);
+                    return $"{i}{Environment.NewLine}{image}";
+                }
+                MoveRobots(robots);
+            }
+            return -1;
+        }
+
+        private void MoveRobots(List<Robot> robots)
+        {
+            foreach (var robot in robots)
+            {
+                var x = (_width + robot.Position.X + robot.Velocity.X) % _width;
+                var y = (_height + robot.Position.Y + robot.Velocity.Y) % _height;
+                robot.Position = new(x, y);
+            }
+        }
+
+        private int CalculateSafetyFactor(List<Robot> robots)
+        {
+            var centerX = _width / 2;
+            var centerY = _height / 2;
+            return robots.Count(robot => robot.Position.X < centerX && robot.Position.Y < centerY) *
+                robots.Count(robot => robot.Position.X > centerX && robot.Position.Y < centerY) *
+                robots.Count(robot => robot.Position.X < centerX && robot.Position.Y > centerY) *
+                robots.Count(robot => robot.Position.X > centerX && robot.Position.Y > centerY);
+        }
+
+        private static bool RobotsClustered(List<Robot> robots)
+        {
+            var positions = new HashSet<Coords>(robots.Select(robot => robot.Position));
+            var multiNeighborCount = 0;
+            foreach (var robot in robots)
+            {
+                if (robot.Position.Neighbors.Count(positions.Contains) > 1)
+                {
+                    multiNeighborCount++;
+                }
+            }
+            return multiNeighborCount > (robots.Count / 5);
+        }
+
+        private string GenerateImage(List<Robot> robots)
+        {
+            var grid = new bool[_height][];
+            for (int y = 0; y < grid.Length; y++)
+            {
+                grid[y] = new bool[_width];
+            }
+            foreach (var robot in robots)
+            {
+                grid[robot.Position.Y][robot.Position.X] = true;
+            }
+            return string.Concat(grid.Select(row => Environment.NewLine + string.Concat(row.Select(value => value ? '█' : ' '))));
+        }
+
+        private static List<Robot> ToRobots(string[] lines)
+        {
+            var robots = new List<Robot>();
+            foreach (var line in lines)
+            {
+                var (position, velocity) = line[2..].SplitInTwo(" v=");
+                var (positionX, positionY) = position.SplitInTwo(',');
+                var (velocityX, velocityY) = velocity.SplitInTwo(',');
+                robots.Add(new(new(int.Parse(positionX), int.Parse(positionY)), new(int.Parse(velocityX), int.Parse(velocityY))));
+            }
+            return robots;
+        }
+
+        private record class Robot(Coords Position, Coords Velocity)
+        {
+            public Coords Position { get; set; } = Position;
+        }
+    }
+}
