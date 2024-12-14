69c648b464de66a21848d86d75e2b5692c4ad712
MatHack <realmathack@users.noreply.github.com>
2024-12-14 12:45:18 +0100

    Day 13 of 2024

---
+++ AdventOfCode.Y2024.Tests/Test13.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Y2024.Tests
+{
+    public class Test13
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day13();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(480L, result);
+        }
+
+        private const string _input = @"Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+";
+    }
+}
---
+++ AdventOfCode.Y2024/Solvers/Day13.cs
@@ -0,0 +1,61 @@
+﻿using Coords = AdventOfCode.Coords<long>;
+
+namespace AdventOfCode.Y2024.Solvers
+{
+    public class Day13 : SolverWithLineGroups
+    {
+        public override object SolvePart1(string[] input) => ToClawMachines(input).Sum(machine => CalculateTokens(machine, 0L));
+        public override object SolvePart2(string[] input) => ToClawMachines(input).Sum(machine => CalculateTokens(machine, 10_000_000_000_000));
+
+        private static long CalculateTokens(ClawMachine machine, long offset = 0)
+        {
+            var Ax = Convert.ToDouble(machine.ButtonA.X);
+            var Bx = machine.ButtonB.X;
+            var Px = machine.Prize.X + offset;
+            var Ay = machine.ButtonA.Y;
+            var By = machine.ButtonB.Y;
+            var Py = machine.Prize.Y + offset;
+            /* Math:
+             *  1) a * Ax + b * Bx = Px
+             *  2) a * Ax = Px - b * Bx                                     1) - b * Bx
+             *  3) a = (Px - b * Bx) / Ax                                   2) / Ax
+             *  4) a * Ay + b * By = Py
+             *  5) ((Px - b * Bx) / Ax) * Ay + b * By = Py                  4) with 'a' replaced by 3)
+             *  6) (Px / Ax - b * Bx / Ax) * Ay + b * By = Py               5) / Ax split over substraction ║ (60 - 40) / 20 == 60 / 20 - 40 / 20
+             *  7) Px * Ay / Ax - (b * Bx * Ay) / Ax + b * By = Py          6) * Ay worked into division
+             *  8) - (b * Bx * Ay) / Ax + b * By = Py - Px * Ay / Ax        7) - (Px * Ay / Ax)
+             *  9) b * By - (b * Bx * Ay) / Ax = Py - Px * Ay / Ax          8) commutative property of +
+             * 10) b * (By - Bx * Ay / Ax) = Py - Px * Ay / Ax              9) merge 'b's ║ (b * Bx * Ay) / Ax == b * (Bx * Ay / Ax)
+             * 11) b = (Py - Px * Ay / Ax) / (By - Bx * Ay / Ax)            10) / By - Bx * Ay / Ax
+             */
+            var b = (Py - Px * Ay / Ax) / (By - Bx * Ay / Ax); // 11) from above
+            var a = (Px - b * Bx) / Ax; // 4) from above
+            var longA = (long)Math.Round(a);
+            var longB = (long)Math.Round(b);
+            if (longA * Ax + longB * Bx == Px && longA * Ay + longB * By == Py)
+            {
+                return longA * 3 + longB;
+            }
+            return 0L;
+        }
+
+        private static List<ClawMachine> ToClawMachines(string[] groups)
+        {
+            var machines = new List<ClawMachine>();
+            for (int i = 0; i < groups.Length; i++)
+            {
+                var lines = groups[i].SplitIntoLines();
+                var (left, right) = lines[0][12..].SplitInTwo(", Y+");
+                var offsetA = new Coords(int.Parse(left), int.Parse(right));
+                (left, right) = lines[1][12..].SplitInTwo(", Y+");
+                var offsetB = new Coords(int.Parse(left), int.Parse(right));
+                (left, right) = lines[2][9..].SplitInTwo(", Y=");
+                var offsetPrize = new Coords(int.Parse(left), int.Parse(right));
+                machines.Add(new(i, offsetA, offsetB, offsetPrize));
+            }
+            return machines;
+        }
+
+        private readonly record struct ClawMachine(int Index, Coords ButtonA, Coords ButtonB, Coords Prize);
+    }
+}
