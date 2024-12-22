49ae3478694e88061b0f50b9e89cca9be7c8760a
MatHack <realmathack@users.noreply.github.com>
2024-12-22 14:29:33 +0100

    Day 19 of 2024

---
+++ AdventOfCode.Y2024.Tests/Test19.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode.Y2024.Tests
+{
+    public class Test19
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day19();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day19();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(16L, result);
+        }
+
+        private const string _input = @"r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
+";
+    }
+}
---
+++ AdventOfCode.Y2024/Solvers/Day19.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode.Y2024.Solvers
+{
+    public class Day19 : SolverWithLineGroups
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var towels = input[0].Split(", ");
+            var designs = input[1].SplitIntoLines();
+            var memo = new Dictionary<string, bool>() { [""] = true };
+            return designs.Count(design => HasPossibleSolution(design, towels, memo));
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var towels = input[0].Split(", ");
+            var designs = input[1].SplitIntoLines();
+            var memo = new Dictionary<string, long>() { [""] = 1L };
+            return designs.Sum(design => CountPossibleSolutions(design, towels, memo));
+        }
+
+        private static bool HasPossibleSolution(string design, string[] towels, Dictionary<string, bool> memo)
+        {
+            // https://en.wikipedia.org/wiki/Memoization
+            if (memo.TryGetValue(design, out var cachedResult))
+            {
+                return cachedResult;
+            }
+            foreach (var towel in towels)
+            {
+                if (towel.Length <= design.Length && design[0..towel.Length] == towel)
+                {
+                    if (HasPossibleSolution(design[towel.Length..], towels, memo))
+                    {
+                        memo.TryAdd(design[towel.Length..], true);
+                        return true;
+                    }
+                }
+            }
+            memo.TryAdd(design, false);
+            return false;
+        }
+
+        private static long CountPossibleSolutions(string design, string[] towels, Dictionary<string, long> memo)
+        {
+            // https://en.wikipedia.org/wiki/Memoization
+            if (memo.TryGetValue(design, out var cachedResult))
+            {
+                return cachedResult;
+            }
+            var count = 0L;
+            foreach (var towel in towels)
+            {
+                if (towel.Length <= design.Length && design[0..towel.Length] == towel)
+                {
+                    count += CountPossibleSolutions(design[towel.Length..], towels, memo);
+                }
+            }
+            memo.TryAdd(design, count);
+            return count;
+        }
+    }
+}
