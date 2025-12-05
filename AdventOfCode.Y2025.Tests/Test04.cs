b66daeb916bd56c70b6927c3475aa22e63a5b2d1
MatHack <realmathack@users.noreply.github.com>
2025-12-05 15:27:14 +0100

    Day 4 of 2025

---
+++ AdventOfCode.Y2025.Tests/Test04.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test04
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day04();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(13, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day04();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(43, result);
+        }
+
+        private const string _input = @"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day04.cs
@@ -0,0 +1,54 @@
+﻿using Coords = AdventOfCode.Coords<int>;
+
+namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day04 : SolverWithCharGrid
+    {
+        public override object SolvePart1(char[][] input)
+        {
+            var total = 0;
+            for (int y = 0; y < input.Length; y++)
+            {
+                for (int x = 0; x < input[y].Length; x++)
+                {
+                    if (input[y][x] == '@' && new Coords(x, y).Adjacents.Count(coord => IsPaperRoll(input, coord)) < 4)
+                    {
+                        total++;
+                    }
+                }
+            }
+            return total;
+        }
+
+        public override object SolvePart2(char[][] input)
+        {
+            var total = 0;
+            var startTotal = 0;
+            do
+            {
+                startTotal = total;
+                for (int y = 0; y < input.Length; y++)
+                {
+                    for (int x = 0; x < input[y].Length; x++)
+                    {
+                        if (input[y][x] == '@' && new Coords(x, y).Adjacents.Count(coord => IsPaperRoll(input, coord)) < 4)
+                        {
+                            input[y][x] = '.';
+                            total++;
+                        }
+                    }
+                }
+            } while (startTotal != total);
+            return total;
+        }
+
+        private static bool IsPaperRoll(char[][] input, Coords coord)
+        {
+            if (coord.Y < 0 || coord.X < 0 || coord.Y >= input.Length || coord.X >= input[0].Length)
+            {
+                return false;
+            }
+            return input[coord.Y][coord.X] == '@';
+        }
+    }
+}
