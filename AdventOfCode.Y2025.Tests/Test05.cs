1d215fc638a9fe9586ec8f603cf25ae5b22af316
MatHack <realmathack@users.noreply.github.com>
2025-12-05 17:41:24 +0100

    Day 5 of 2025

---
+++ AdventOfCode.Y2025.Tests/Test05.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test05
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day05();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day05();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(14L, result);
+        }
+
+        private const string _input = @"3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day05.cs
@@ -0,0 +1,59 @@
+﻿using Range = AdventOfCode.Range<long>;
+
+namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day05 : SolverWithLineGroups
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var (freshRanges, ingredients) = ToFreshRangesAndIngredients(input);
+            var total = 0;
+            foreach (var ingredient in ingredients)
+            {
+                foreach (var range in freshRanges)
+                {
+                    if (range.Start <= ingredient && ingredient <= range.End)
+                    {
+                        total++;
+                        break;
+                    }
+                    if (range.Start > ingredient)
+                    {
+                        break;
+                    }
+                }
+            }
+            return total;
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var (freshRanges, _) = ToFreshRangesAndIngredients(input);
+            var total = 0L;
+            var last = 0L;
+            foreach (var range in freshRanges)
+            {
+                if (range.End <= last)
+                {
+                    continue;
+                }
+                total += 1L + range.End - Math.Max(range.Start, last + 1L);
+                last = range.End;
+            }
+            return total;
+        }
+
+        private static (Range[] FreshRanges, long[] Ingredients) ToFreshRangesAndIngredients(string[] lineGroups)
+        {
+            var freshRanges = lineGroups[0].SplitIntoLines()
+                .Select(line => line.SplitInTwo('-'))
+                .Select(ids => new Range(long.Parse(ids.Left), long.Parse(ids.Right)))
+                .OrderBy(range => range.Start)
+                .ToArray();
+            var ingredients = lineGroups[1].SplitIntoLines()
+                .Select(long.Parse)
+                .ToArray();
+            return (freshRanges, ingredients);
+        }
+    }
+}
