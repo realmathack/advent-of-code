c04dcf3e3c66c8eac14f957eab6b1f6e6df509b2
MatHack <realmathack@users.noreply.github.com>
2025-12-05 14:09:49 +0100

    Day 3 of 2025

---
+++ AdventOfCode.Y2025.Tests/Test03.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test03
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day03();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(357L, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day03();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(3121910778619L, result);
+        }
+
+        private const string _input = @"987654321111111
+811111111111119
+234234234234278
+818181911112111
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day03.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day03 : SolverWithLines
+    {
+        public override object SolvePart1(string[] input) => input.Sum(line => GetMaxJoltage(line, 2));
+        public override object SolvePart2(string[] input) => input.Sum(line => GetMaxJoltage(line, 12));
+
+        private static long GetMaxJoltage(ReadOnlySpan<char> bank, int digitCount)
+        {
+            var max = new char[digitCount];
+            var currentDigit = 0;
+            var nextStartPos = 0;
+            while (currentDigit < digitCount)
+            {
+                var currentMax = '0';
+                var currentMaxPos = 0;
+                for (int i = nextStartPos; i <= bank.Length - (digitCount - currentDigit); i++)
+                {
+                    if (bank[i] > currentMax)
+                    {
+                        currentMax = bank[i];
+                        currentMaxPos = i;
+                    }
+                }
+                max[currentDigit++] = currentMax;
+                nextStartPos = currentMaxPos + 1;
+            }
+            return long.Parse(max.AsSpan());
+        }
+    }
+}
