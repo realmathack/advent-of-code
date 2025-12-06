62321080aa6eebe3c52ecda64d5822b02ac8f57a
MatHack <realmathack@users.noreply.github.com>
2025-12-06 09:53:34 +0100

    Day 6 of 2025

---
+++ AdventOfCode.Y2025.Tests/Test06.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test06
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day06();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(4277556L, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day06();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(3263827L, result);
+        }
+
+        private const string _input = @"123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day06.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day06 : SolverWithLines
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var numbers = input[..^1].Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray()).ToArray();
+            var operators = input[^1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var total = 0L;
+            for (int i = 0; i < operators.Length; i++)
+            {
+                var tmp = numbers.Select(line => line[i]);
+                total += (operators[i] == "+") ? tmp.Sum() : tmp.Product();
+            }
+            return total;
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var total = 0L;
+            var numbers = new List<long>();
+            var op = input[^1][0];
+            for (int i = 0; i < input[^1].Length; i++)
+            {
+                var number = string.Concat(input[..^1].Select(line => line[i]).Where(c => c != ' '));
+                if (number != string.Empty)
+                {
+                    numbers.Add(long.Parse(number));
+                }
+                if (number == string.Empty || i == (input[^1].Length - 1))
+                {
+                    total += (op == '+') ? numbers.Sum() : numbers.Product();
+                }
+                if (number == string.Empty)
+                {
+                    numbers.Clear();
+                    op = input[^1][i + 1];
+                }
+            }
+            return total;
+        }
+    }
+}
