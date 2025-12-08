f08c8eb58e9baa5792ac6891d7ba65c9e701653b
MatHack <realmathack@users.noreply.github.com>
2025-12-08 11:34:23 +0100

    Day 8 of 2025

---
+++ AdventOfCode.Y2025.Tests/Test08.cs
@@ -0,0 +1,49 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test08
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day08(10);
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(40, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day08();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(25272L, result);
+        }
+
+        private const string _input = @"162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day08.cs
@@ -0,0 +1,99 @@
+﻿using Coords3D = AdventOfCode.Coords3D<int>;
+
+namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day08(int connections) : SolverWithLines
+    {
+        public Day08() : this(1000) { }
+
+        public override object SolvePart1(string[] input)
+        {
+            var boxes = ToJunctionBoxes(input);
+            var distances = CalculateDistances(boxes);
+            var groups = new List<HashSet<Coords3D>>();
+            foreach (var (a, b, _) in distances.Take(connections))
+            {
+                ConnectBoxes(a, b, groups);
+            }
+            var product = groups.Select(group => group.Count).OrderDescending().Take(3).Product();
+            return product;
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var boxes = ToJunctionBoxes(input);
+            var distances = CalculateDistances(boxes);
+            var groups = new List<HashSet<Coords3D>>();
+            foreach (var (a, b, _) in distances)
+            {
+                ConnectBoxes(a, b, groups);
+                if (groups.Count == 1 && groups[0].Count == boxes.Length)
+                {
+                    return (long)a.X * b.X;
+                }
+            }
+            return 0L;
+        }
+
+        private static void ConnectBoxes(Coords3D a, Coords3D b, List<HashSet<Coords3D>> groups)
+        {
+            var groupA = groups.FirstOrDefault(group => group.Contains(a));
+            var groupB = groups.FirstOrDefault(group => group.Contains(b));
+            if (groupA is null)
+            {
+                if (groupB is null)
+                {
+                    groups.Add([a, b]);
+                }
+                else
+                {
+                    groupB.Add(a);
+                }
+            }
+            else
+            {
+                if (groupB is null)
+                {
+                    groupA.Add(b);
+                }
+                else if (groupA != groupB)
+                {
+                    groupA.UnionWith(groupB);
+                    groups.Remove(groupB);
+                }
+            }
+        }
+
+        private static IEnumerable<(Coords3D<int> A, Coords3D<int> B, double Distance)> CalculateDistances(Coords3D<int>[] boxes)
+        {
+            var distances = new List<(Coords3D A, Coords3D B, double Distance)>();
+            for (int a = 0; a < boxes.Length; a++)
+            {
+                for (int b = a + 1; b < boxes.Length; b++)
+                {
+                    distances.Add((boxes[a], boxes[b], CalculateDistance(boxes[a], boxes[b])));
+                }
+            }
+            return distances.OrderBy(distance => distance.Distance);
+        }
+
+        private static double CalculateDistance(Coords3D a, Coords3D b)
+        {
+            long x = a.X - b.X;
+            long y = a.Y - b.Y;
+            long z = a.Z - b.Z;
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        private static Coords3D[] ToJunctionBoxes(string[] lines)
+        {
+            var boxes = new Coords3D[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var coords = lines[i].Split(',');
+                boxes[i] = new(int.Parse(coords[0]), int.Parse(coords[1]), int.Parse(coords[2]));
+            }
+            return boxes;
+        }
+    }
+}
