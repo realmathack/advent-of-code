477cb2904d76afdd17a8af42eeaa0e7aa268fea1
MatHack <realmathack@users.noreply.github.com>
2025-12-09 13:53:05 +0100

    Day 9A of 2025

---
+++ AdventOfCode.Y2025.Tests/Test09.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test09
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day09();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(50L, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day09();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(24L, result);
+        }
+
+        private const string _input = @"7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day09.cs
@@ -0,0 +1,38 @@
+﻿using Coords = AdventOfCode.Coords<int>;
+
+namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day09 : SolverWithLines
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var tiles = input.Select(Coords.Parse).ToArray();
+            var largest = 0L;
+            for (int a = 0; a < tiles.Length; a++)
+            {
+                for (int b = a + 1; b < tiles.Length; b++)
+                {
+                    var area = CalculateArea(tiles[a], tiles[b]);
+                    if (area > largest)
+                    {
+                        largest = area;
+                    }
+                }
+            }
+            return largest;
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            // TODO: Implement (create minimized version of grid? -> https://github.com/maksverver/AdventOfCode/blob/master/2025/09.py)
+            return null!;
+        }
+
+        private static long CalculateArea(Coords a, Coords b)
+        {
+            var height = 1L + Math.Abs(a.Y - b.Y);
+            var width = 1L + Math.Abs(a.X - b.X);
+            return height * width;
+        }
+    }
+}
