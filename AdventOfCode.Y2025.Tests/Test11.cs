096e2e25530de7e3789654c57f929dda2b58ba4c
MatHack <realmathack@users.noreply.github.com>
2025-12-12 14:25:14 +0100

    Day 11A of 2025

---
+++ AdventOfCode.Y2025.Tests/Test11.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test11
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day11();
+            subject.SetInput(_input1);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day11();
+            subject.SetInput(_input2);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(2, result);
+        }
+
+        private const string _input1 = @"aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+";
+        private const string _input2 = @"svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day11.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode.Y2025.Solvers
+{
+    public class Day11 : SolverWithLines
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var devices = ToDevices(input);
+            return FindPathCount(devices["you"], devices["out"]);
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            var devices = ToDevices(input);
+            var start = devices["svr"];
+            var dac = devices["dac"];
+            var fft = devices["fft"];
+            var end = devices["out"];
+            // TODO: Implement
+            return 0;
+        }
+
+        private static int FindPathCount(Device current, Device end)
+        {
+            if (current == end)
+            {
+                return 1;
+            }
+            var count = 0;
+            foreach (var output in current.Outputs)
+            {
+                count += FindPathCount(output, end);
+            }
+            return count;
+        }
+
+        private static Dictionary<string, Device> ToDevices(string[] lines)
+        {
+            var devices = new Dictionary<string, Device>();
+            foreach (var line in lines)
+            {
+                var id = line[0..3];
+                if (!devices.TryGetValue(id, out var device))
+                {
+                    device = new(id, []);
+                    devices[id] = device;
+                }
+                var outputIds = line[5..].Split(' ');
+                foreach (var outputId in outputIds)
+                {
+                    if (!devices.TryGetValue(outputId, out var output))
+                    {
+                        output = new(outputId, []);
+                        devices[outputId] = output;
+                    }
+                    device.Outputs.Add(output);
+                }
+            }
+            return devices;
+        }
+
+        private record class Device(string Id, List<Device> Outputs);
+    }
+}
