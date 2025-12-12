cdc70aae0ec2fe35be27e7f1fd2e53de0c2a8707
MatHack <realmathack@users.noreply.github.com>
2025-12-12 14:24:24 +0100

    Day 10A of 2025

---
+++ AdventOfCode.Y2025.Tests/Test10.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode.Y2025.Tests
+{
+    public class Test10
+    {
+        [Fact]
+        public void TestPart1()
+        {
+            var subject = new Day10();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart1();
+
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void TestPart2()
+        {
+            var subject = new Day10();
+            subject.SetInput(_input);
+
+            var result = subject.SolvePart2();
+
+            Assert.Equal(null!, result);
+        }
+
+        private const string _input = @"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+";
+    }
+}
---
+++ AdventOfCode.Y2025/Solvers/Day10.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Y2025.Solvers
+{
+    public partial class Day10 : SolverWithLines
+    {
+        public override object SolvePart1(string[] input)
+        {
+            var machines = ToMachines(input);
+            return machines.Sum(FindFewestButtonPresses);
+        }
+
+        public override object SolvePart2(string[] input)
+        {
+            // TODO:
+            // https://old.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+            return null!;
+        }
+
+        private static int FindFewestButtonPresses(Machine machine)
+        {
+            var queue = new Queue<ButtonPressed>();
+            queue.Enqueue(new(0, []));
+            while (queue.TryDequeue(out var current))
+            {
+                if (machine.Indicators == current.Lights)
+                {
+                    return current.Pressed.Count;
+                }
+                for (int i = 0; i < machine.Buttons.Count; i++)
+                {
+                    if (current.Pressed.Contains(i)) // Pressing buttons more than once is not useful (0 ^ 1 ^ 1 == 0)
+                    {
+                        continue;
+                    }
+                    var afterPress = PressButtons(current.Lights, machine.Buttons[i], machine.Joltages.Length - 1);
+                    var pressed = new HashSet<int>(current.Pressed) { i };
+                    queue.Enqueue(new(afterPress, pressed));
+                }
+            }
+            return int.MaxValue;
+        }
+
+        private static int PressButtons(int lights, int[] buttons, int lightCount)
+        {
+            foreach (var button in buttons)
+            {
+                lights ^= 1 << (lightCount - button);
+            }
+            return lights;
+        }
+
+        private static Machine[] ToMachines(string[] lines)
+        {
+            var machines = new Machine[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var match = MachineRegex().Match(lines[i]);
+                var indicators = Convert.ToInt32(match.Groups[1].Value.Replace('#', '1').Replace('.', '0'), 2);
+                var buttons = match.Groups[2].Value.Split(' ').Select(group => group[1..^1].Split(',').Select(int.Parse).ToArray()).ToList();
+                var joltages = match.Groups[3].Value.Split(',').Select(int.Parse).ToArray();
+                machines[i] = new(indicators, buttons, joltages);
+            }
+            return machines;
+        }
+
+        [GeneratedRegex(@"\[([.#]+)\] (.+) \{([\d,]+)\}")]
+        private static partial Regex MachineRegex();
+
+        private record class Machine(int Indicators, List<int[]> Buttons, int[] Joltages);
+        private record class ButtonPressed(int Lights, HashSet<int> Pressed);
+    }
+}
